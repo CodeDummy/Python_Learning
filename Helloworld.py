df1bc17ece66cc38e6d586940d91ef9b4fe3ea6e
CodeDummy <kl17a9161@gmail.com>
2016-03-19 10:46:40 +0100

    if-else,function.enumerate examples

--- Helloworld.py
+++ Helloworld.py
@@ -1,10 +1,25 @@
 #Helloworld.py
 
+def Whtnum (List1):
+#Learning if Else
+    if List1[1] != 13:
+        print ('Not 13')
+    else :
+        print('13')
 
+
+#for loop range example
+print('For loop and xrange example ; Print \"Hello World\" 5 times')
 for i in xrange(5):
     print ('Hello World')
 
 List1= [12,13,14,15]
-print List1
+
+print('Enumerate example')
+for i, num in enumerate (List1):
+    print (num)
+print('Function with if-else example')
+Whtnum(List1)
+
 
     
