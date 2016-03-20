952a83395b4769864307249fc287d9234026d79b
CodeDummy <kl17a9161@gmail.com>
2016-03-20 12:13:18 +0100

    string2.py done

--- GoogleExercises.py
+++ GoogleExercises.py
@@ -83,8 +83,57 @@ def verbing(s):
   else:
       return s
 
+  
+# F. not_bad
+# Given a string, find the first appearance of the
+# substring 'not' and 'bad'. If the 'bad' follows
+# the 'not', replace the whole 'not'...'bad' substring
+# with 'good'.
+# Return the resulting string.
+# So 'This dinner is not that bad!' yields:
+# This dinner is good!
+def not_bad(s):
+ 
+ # split=s.split()
+  
+  ndx=s.find('not') #index of not 
+  bdx=s.find('bad')  #index of bad 
 
+  if (ndx<bdx):
+      return s[:ndx]+'good'+s[bdx+3:]
+  else :
+      return s
 
+  
+# F. front_back
+# Consider dividing a string into two halves.
+# If the length is even, the front and back halves are the same length.
+# If the length is odd, we'll say that the extra char goes in the front half.
+# e.g. 'abcde', the front half is 'abc', the back half 'de'.
+# Given 2 strings, a and b, return a string of the form
+#  a-front + b-front + a-back + b-back
+def front_back(a, b):
+ 
+  la=len(a)
+  lb=len(b)
+  if (la%2):
+      aflim=((la-1)//2)+1
+      afront=a[:aflim]
+      aback=a[aflim:]
+  else:
+      aflim=(la//2)
+      afront=a[:aflim]
+      aback=a[aflim:]
+
+  if (lb%2):
+      bflim=((lb-1)//2)+1
+      bfront=b[:bflim]
+      bback=b[bflim:]
+  else:
+       bflim=(lb//2)
+       bfront=b[:bflim]
+       bback=b[bflim:]
+  return afront+bfront+aback+bback
 
 
 
@@ -125,6 +174,18 @@ def main():
   test(verbing('swiming'), 'swimingly')
   test(verbing('do'), 'do')
 
+  print
+  print ('not_bad')
+  test(not_bad('This movie is not so bad'), 'This movie is good')
+  test(not_bad('This dinner is not that bad!'), 'This dinner is good!')
+  test(not_bad('This tea is not hot'), 'This tea is not hot')
+  test(not_bad("It's bad yet not"), "It's bad yet not")
+
+  print
+  print ('front_back')
+  test(front_back('abcd', 'xy'), 'abxcdy')
+  test(front_back('abcde', 'xyz'), 'abcxydez')
+  test(front_back('Kitten', 'Donut'), 'KitDontenut')
 
 
 
