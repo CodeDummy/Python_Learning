9850f335309d082e0b01b3a2c80c9811d29b5566
santhosh <kl17a9161@gmail.com>
2016-05-19 16:58:36 +0200

    Current time and circle area complete

--- w3_exercises.py
+++ w3_exercises.py
@@ -2,3 +2,15 @@
 #get python version
 import sys
 print (sys.version)
+
+#display current date and time 
+
+import datetime #time module is better to get time wrt epoch
+now = datetime.datetime.now()
+print ("Current date and time : ")
+print (now.strftime("%Y-%m-%d %H:%M:%S"))
+
+#area of a circle with user input
+from math import pi
+r = float(input ("Input the radius of the circle : ")) #interactive user input instead of sysarg
+print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))g
